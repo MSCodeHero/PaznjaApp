b1965d9bac955730797a39b64e659947a9251d0b
MSCodeHero <38673610+MSCodeHero@users.noreply.github.com>
2023-12-18 23:22:21 +0100

    First Map shown

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Text} from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+
+  useEffect(() => {
+    console.log('position is now: ', latitude, longitude);
+  }, [latitude]);
+
+  useEffect(() => {
+    console.log('position is now: ', latitude, longitude);
+  }, [longitude]);
+
+  const handleLongPress = event => {
+    const coordinates = event.nativeEvent.coordinate;
+    setLatitude(coordinates.latitude)
+    setLongitude(coordinates.longitude)
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>HELLO. ITS ME. YO BOY. PERICA# X:{latitude} Y:{longitude}</Text>
+      <MapView style={styles.map} onLongPress={handleLongPress} />
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    position: 'relative',
+    top:'30%',
+    left:'40%',
+    color: 'green',
+    zIndex: 999
+  }
 });
